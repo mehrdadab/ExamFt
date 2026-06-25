929882cea9e3f67d369edc7a64a03bb8737c6a2d
mehrdad <mehrdadab@yahoo.com>
2026-06-25 16:15:28 +1000

    add dbcontext

--- src/Services/Identity/Program.cs
+++ src/Services/Identity/Program.cs
@@ -1,8 +1,16 @@
+using ExamFT.Identity.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.AddNpgsqlDbContext<ApplicationDbContext>("identitydb", configureDbContextOptions: options =>
+{
+    options.UseSnakeCaseNamingConvention();
+});
 
 var app = builder.Build();
 
