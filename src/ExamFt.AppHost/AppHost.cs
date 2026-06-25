60f1f94d262b5842e44b12fe79517ae3811866f5
mehrdad <mehrdadab@yahoo.com>
2026-06-25 10:56:54 +1000

    add identity service

--- src/ExamFt.AppHost/AppHost.cs
+++ src/ExamFt.AppHost/AppHost.cs
@@ -3,6 +3,11 @@ var builder = DistributedApplication.CreateBuilder(args);
 var postgres = builder.AddPostgres("postgres");
 var identityDb = postgres.AddDatabase("identitydb");
 
+builder.AddProject<Projects.ExamFT_Identity>("identity")
+    .WithHttpHealthCheck("/health")
+    .WithReference(identityDb)
+    .WaitFor(identityDb);
+
 var apiService = builder.AddProject<Projects.ExamFt_ApiService>("apiservice")
     .WithHttpHealthCheck("/health");
 
